87ac80adb104a3bc08053fcf6127bc40b8a73c8b
Joseph Roque <contact@josephroque.ca>
2019-03-17 12:03:48 -0400

    Initial vapor commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,14 @@
+//
+//  LinuxMain.swift
+//  AppTests
+//
+//  Created by Joseph Roque on 2019-03-17
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import XCTest
+import AppTests
+
+var tests: [XCTestCaseEntry] = []
+tests += AppTests.allTests()
+XCTMain(tests)
