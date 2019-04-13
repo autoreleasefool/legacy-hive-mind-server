bd327dd7bb324a5a293dd09ed229511349800309
Joseph Roque <contact@josephroque.ca>
2019-04-13 19:10:23 -0400

    Remove Starscream dependency, in favour of Vapor/WebSocket

--- Package.swift
+++ Package.swift
@@ -8,11 +8,10 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
-		.package(url: "https://github.com/josephroquedev/hive-engine.git", .branch("master")),
-		.package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0")
+		.package(url: "https://github.com/josephroquedev/hive-engine.git", .branch("master"))
 	],
 	targets: [
-		.target(name: "App", dependencies: ["Vapor", "HiveEngine", "Starscream"]),
+		.target(name: "App", dependencies: ["Vapor", "HiveEngine"]),
 		.target(name: "Run", dependencies: ["App"]),
 		.testTarget(name: "AppTests", dependencies: ["App"])
 	]
