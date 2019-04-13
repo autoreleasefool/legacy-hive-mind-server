9c902a54e44be2ab3962ffd9ac76c8860bfbe1b0
Joseph Roque <contact@josephroque.ca>
2019-04-12 21:26:48 -0400

    Add Starscream as a dependency

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
-		.package(url: "https://github.com/josephroquedev/hive-engine.git", .branch("master"))
+		.package(url: "https://github.com/josephroquedev/hive-engine.git", .branch("master")),
+		.package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0")
 	],
 	targets: [
-		.target(name: "App", dependencies: ["Vapor", "HiveEngine"]),
+		.target(name: "App", dependencies: ["Vapor", "HiveEngine", "Starscream"]),
 		.target(name: "Run", dependencies: ["App"]),
 		.testTarget(name: "AppTests", dependencies: ["App"])
 	]
